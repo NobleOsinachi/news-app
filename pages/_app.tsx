1388f0d36ae2c3dc96a6b69fdac867b3b9b31ecf
Noble Osinachi <nobleosinachi98@gmail.com>
2023-07-23 14:56:01 +0100

    Set up project for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-    return (  );
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Next.js App</title>
+                {/* Add any other meta tags or link tags you need */}
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
-export default App;
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-const Home = () => {
-    return (
+import Head from 'next/head';
 
+const Home: React.FC = () => {
+    return (
         <div>
-            <h2>Home Page</h2>
+            <Head>
+                <title>New Blog using Next JS</title>
+                <meta name="description" content="This blog fetches latest news from the News API." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* Your page content goes here */}
+            <main>
+                <h1>Welcome to the Default Page</h1>
+                <p>This is the content of the default index page.</p>
+            </main>
+
+            {/* Add any other components, footer, etc. */}
         </div>
     );
 };
 
 export default Home;
+
+
+
+
